01be268303bcb7cf021c726bccc4149f0c75f954
maks <maxtwain777@gmail.com>
2023-05-15 19:56:11 +0300

    feat: icon design

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Item from "./components/Item.vue";
+</script>
+
+<template>
+  <header class="header">
+    <div class="header-wrapper">
+      <h1 class="header-title">ToDo List</h1>
+    </div>
+  </header>
+  <div class="wrapper">
+    <h1 class="title">ToDo List</h1>
+    <div class="add-task"></div>
+    <div class="todo-wrapper">
+      <Item />
+      <Item />
+      <Item />
+    </div>
+  </div>
+</template>
+<style src="./styles/main.css"></style>
---
+++ vue-todo/src/components/Item.vue
@@ -0,0 +1,23 @@
+import '@/styles/item.css';
+<script>
+export default {
+  name: "Item",
+};
+</script>
+
+<template>
+  <div class="todo-item">
+    <label class="item-label" for="checkbox">
+      <input class="item-checkbox" type="checkbox" id="checkbox" />
+      <span class="item-text">todotodotodo</span>
+    </label>
+    <img src="../assets/edit-icon.png" alt="" class="item-edit" />
+    <img src="../assets/delete-icon.png" alt="" class="item-delete" />
+  </div>
+</template>
+<style src="../styles/item.css"></style>
+
+<!-- <label class="item-label" for="checkboxInput">
+      <input class="item-checkbox" type="checkbox" id="checkboxInput" />
+      <span class="item-text">todotodotodo</span>
+    </label> -->
